47ab7106a22dae9f4c2174951ac8ccb24513e2d3
Antonio Wills <antontiowillissilvasantos@gmail.com>
2024-04-10 07:56:20 -0300

    Mudando a cor da janela principal

--- Tkinter/Calculadora_Matrizes_tkinter/main.py
+++ Tkinter/Calculadora_Matrizes_tkinter/main.py
@@ -8,34 +8,37 @@ from Funcao.matriz import printResultado
 from Funcao.title import printTitle
 from Funcao.comandoBotao import *
 
+
 def printButtons(all_buttons_frame):
-    botao_soma = Botao('Soma : A + B', lambda : comandoSomarMatrizes(resultado_frame, 
-                                                                     receber_matrizA.getMatriz(),
-                                                                     receber_matrizB.getMatriz()))
-    botao_menos = Botao('Menos : A - B', lambda : comandoSubtrairtMatrizes(resultado_frame,
-                                                                           receber_matrizA.getMatriz(),
-                                                                           receber_matrizB.getMatriz()))
-    botao_mult = Botao('Mult : A * B', lambda : comandoMultMatrizes(resultado_frame, 
+    botao_soma = Botao('Soma : A + B', lambda: comandoSomarMatrizes(resultado_frame,
                                                                     receber_matrizA.getMatriz(),
                                                                     receber_matrizB.getMatriz()))
-    botao_sistema = Botao('Sistema : Ax = B', lambda : comandoResolverSistemaAxB(resultado_frame,
-                                                                         receber_matrizA.getMatriz(),
-                                                                         receber_matrizB.getMatriz()))
-    botao_matriz_magica = Botao('Matriz Mágica', lambda : comandoMatrizMagica(resultado_frame))
-    
+    botao_menos = Botao('Menos : A - B', lambda: comandoSubtrairtMatrizes(resultado_frame,
+                                                                          receber_matrizA.getMatriz(),
+                                                                          receber_matrizB.getMatriz()))
+    botao_mult = Botao('Mult : A * B', lambda: comandoMultMatrizes(resultado_frame,
+                                                                   receber_matrizA.getMatriz(),
+                                                                   receber_matrizB.getMatriz()))
+    botao_sistema = Botao('Sistema : Ax = B', lambda: comandoResolverSistemaAxB(resultado_frame,
+                                                                                receber_matrizA.getMatriz(),
+                                                                                receber_matrizB.getMatriz()))
+    botao_matriz_magica = Botao(
+        'Matriz Mágica', lambda: comandoMatrizMagica(resultado_frame))
+
     botao_soma.printBotaoPack(all_buttons_frame)
     botao_menos.printBotaoPack(all_buttons_frame)
     botao_mult.printBotaoPack(all_buttons_frame)
     botao_sistema.printBotaoPack(all_buttons_frame)
     botao_matriz_magica.printBotaoPack(all_buttons_frame)
 
+
 janela_principal = tk.Tk()
 janela_principal.title('Matriz')
-janela_principal.configure(borderwidth=4, relief="solid", bg='red')
+janela_principal.configure(borderwidth=4, relief="solid", bg='#AFEEEE')
 
 conteudo_geral = Frame(janela_principal)
 conteudo_geral.configure(borderwidth=2, relief="solid")
-conteudo_geral.pack(pady=10,padx=10)
+conteudo_geral.pack(pady=10, padx=10)
 
 printTitle(conteudo_geral)
 
@@ -47,7 +50,8 @@ conteudo.pack(padx=5, pady=5)
 # #B0E0E6
 # #AFEEEE
 
-all_buttons_operatoin_frame = Frame(conteudo, bg='#00ff00', borderwidth=3, relief='raised')
+all_buttons_operatoin_frame = Frame(
+    conteudo, bg='#00ff00', borderwidth=3, relief='raised')
 all_buttons_operatoin_frame.grid(row=0, column=0, pady=3, padx=10)
 
 printButtons(all_buttons_operatoin_frame)
@@ -59,22 +63,26 @@ frame_matrizA = Frame(frame_tabelas, borderwidth=3, relief='ridge')
 frame_matrizA.pack(side='left', padx=5, pady=5)
 
 receber_matrizA = EntryMatriAndComand(frame_matrizA)
-receber_matrizA.receber('Matriz A : ', 
-                        lambda : comandoDetA(resultado_frame, receber_matrizA.getMatriz()), 
-                        lambda : comandoMatrizTransA(resultado_frame, receber_matrizA.getMatriz()), 
-                        lambda : comandoMatrizInversaA(resultado_frame, receber_matrizA.getMatriz()))
+receber_matrizA.receber('Matriz A : ',
+                        lambda: comandoDetA(
+                            resultado_frame, receber_matrizA.getMatriz()),
+                        lambda: comandoMatrizTransA(
+                            resultado_frame, receber_matrizA.getMatriz()),
+                        lambda: comandoMatrizInversaA(resultado_frame, receber_matrizA.getMatriz()))
 
 frame_matrizB = Frame(frame_tabelas, borderwidth=3, relief='ridge')
 frame_matrizB.pack(side='left', padx=5, pady=5)
 
 receber_matrizB = EntryMatriAndComand(frame_matrizB)
-receber_matrizB.receber('Matriz B : ', 
-                        lambda : comandoDetA(resultado_frame, receber_matrizB.getMatriz()), 
-                        lambda : comandoMatrizTransB(resultado_frame, receber_matrizB.getMatriz()),
-                        lambda : comandoMatrizInversaB(resultado_frame, receber_matrizB.getMatriz()))
+receber_matrizB.receber('Matriz B : ',
+                        lambda: comandoDetA(
+                            resultado_frame, receber_matrizB.getMatriz()),
+                        lambda: comandoMatrizTransB(
+                            resultado_frame, receber_matrizB.getMatriz()),
+                        lambda: comandoMatrizInversaB(resultado_frame, receber_matrizB.getMatriz()))
 
 resultado_frame = Frame(conteudo, bg='#fff', borderwidth=3, relief='raised')
-resultado_frame.grid(row=0, column=2, padx=5,pady=3)
+resultado_frame.grid(row=0, column=2, padx=5, pady=3)
 
 printResultado(resultado_frame, None, True, 'Nenhuma operação realizada')
 
